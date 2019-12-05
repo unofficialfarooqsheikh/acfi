305ec0697dec6d6b69cd6c484e11b8c88a9b7ea8
unofficialfarooqsheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2019-12-05 10:58:13 +0530

    Updates

---
+++ precache-manifest.2de0a4a1ace2003ccab1b8a392f845fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f206a8a46f50c442a7b9ed9ec557fc1",
+    "url": "/yourreponame/index.html"
+  },
+  {
+    "revision": "938991a55e66b9a102e0",
+    "url": "/yourreponame/static/css/main.dff90c5e.chunk.css"
+  },
+  {
+    "revision": "f7f66427b3c41387cd97",
+    "url": "/yourreponame/static/js/2.a60d124a.chunk.js"
+  },
+  {
+    "revision": "938991a55e66b9a102e0",
+    "url": "/yourreponame/static/js/main.efc496de.chunk.js"
+  },
+  {
+    "revision": "e8ebf55132194babac9d",
+    "url": "/yourreponame/static/js/runtime-main.e3b91436.js"
+  }
+]);
