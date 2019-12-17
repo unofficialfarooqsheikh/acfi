5fc9a449a65ae4244a42f39ded7945901785e992
unofficialfarooqsheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2019-12-17 17:12:57 +0530

    added 404 for pages which are not found

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import AcFiLogin from '../AcFiLogin/AcFiLogin';
 import MainPageLayout from '../../components/Layout/MainPageLayout/MainPageLayout';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 
 class MainContainer extends Component {
@@ -12,11 +12,12 @@ class MainContainer extends Component {
 
             <div style={{
                     'fontFamily': "'Merriweather', serif !important'"}}>
+                <Switch>
                 <Route path={"/"} exact component={AcFiLogin}/>
                 <Route path="/mainPage"  component={MainPageLayout}/>
-                
+                <Route path="*"  render={ ()=>{return <h1>404 Page Not Found</h1>}}/>
+                </Switch> 
             </div>
-
         );
     }
 
